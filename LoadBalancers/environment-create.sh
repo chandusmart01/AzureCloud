ab2f0d247e95e2623b6dce9a935033c5c005c6a4
chandusmart01 <51012572+chandusmart01@users.noreply.github.com>
2023-04-15 16:27:09 +0530

    Update environment-create.sh

--- LoadBalancers/environment-create.sh
+++ LoadBalancers/environment-create.sh
@@ -1,27 +1,27 @@
-group=azure-load-balancer-introduction
-az group create -g $group -l northeurope
+group=dev
+az group create -g $group -l eastus
 username=adminuser
 password='SecretPassword123!@#'
 
 az network vnet create \
   -n vm-vnet \
   -g $group \
-  -l northeurope \
+  -l eastus \
   --address-prefixes '192.168.0.0/16' \
   --subnet-name subnet \
   --subnet-prefixes '192.168.1.0/24'
   
 az vm availability-set create \
   -n vm-as \
-  -l northeurope \
+  -l eastus \
   -g $group
 
-for NUM in 1 2 3
+for NUM in 1 2
 do
   az vm create \
     -n vm-eu-0$NUM \
     -g $group \
-    -l northeurope \
+    -l eastus \
     --size Standard_B1s \
     --image Win2019Datacenter \
     --admin-username $username \
@@ -33,12 +33,12 @@ do
 	  --nsg vm-nsg
 done
 
-for NUM in 1 2 3
+for NUM in 1 2
 do
   az vm open-port -g $group --name vm-eu-0$NUM --port 80
 done
 
-for NUM in 1 2 3
+for NUM in 1 2
 do
   az vm extension set \
     --name CustomScriptExtension \
@@ -48,6 +48,4 @@ do
     --version 1.8 \
     --settings '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"}'
 done
-
-
 
